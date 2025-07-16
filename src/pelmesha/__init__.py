ba85b3a8eb708400f12df20e653fe38bbd283ff5
Andrei Kuzin <Kuzya-90@bk.ru>
2025-07-16 15:51:52 +0300

    modified:   pyproject.toml 	modified:   src/pelmesha/__init__.py

--- src/pelmesha/__init__.py
+++ src/pelmesha/__init__.py
@@ -4,6 +4,5 @@ pelmesha
 pelmesha is a Python library for loading, preprocessing, processing, storage and extracting peaklists of Mass Spectrometry Imaging (MSI) data.
 """
 
-__version__ = "0.1.2"
 __author__ = 'Andrey Kuzin'
 __credits__ = 'Moscow Institue of Physics and Technology'
